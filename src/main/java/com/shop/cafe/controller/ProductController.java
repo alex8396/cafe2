47deeaf0962113787fa5d250a930fe47acdcda08
admin <admin@DESKTOP-TEEN611>
2025-03-05 11:30:31 +0900

    첫 구조

---
+++ src/main/java/com/shop/cafe/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.shop.cafe.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shop.cafe.dto.Product;
+import com.shop.cafe.service.ProductService;
+
+@RestController
+@CrossOrigin("http://192.168.219.109:5503")
+public class ProductController {
+	
+	
+	@Autowired
+	ProductService productService;
+	
+	
+	@GetMapping("getAllProducts")
+	public List<Product> getAllProducts(){
+		try {
+			System.out.println("getAllProducts 실행됨");
+			return productService.getAllproducts();
+		} catch (Exception e) {			
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
